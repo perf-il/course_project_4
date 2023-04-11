942b79421587da911eadc74a628f3944b3ec3593
Ilia Perfilov <ind.climb@gmail.com>
2023-04-11 21:31:19 +0300

    docstrings were added

--- classes.py
+++ classes.py
@@ -27,8 +27,14 @@ class GetReguestError(Exception):
 
 
 class HH(Engine):
-
-    def get_request(self, keyword, page):
+    """Класс для парсинга платформы HeadHunter"""
+    def get_request(self, keyword: str, page: int):
+        """
+        метод отправки запроса и получения информации с сайта https://api.hh.ru/vacancies
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param page: текущая страница с результатами
+        :return: список с данными вакансий с текущей страницы или исключение GetReguestError
+        """
         url = "https://api.hh.ru/vacancies"
         params = {
             "text": keyword,
@@ -42,6 +48,12 @@ class HH(Engine):
             raise GetReguestError(response.status_code, 'HeadHunter')
 
     def get_vacancies(self, keyword: str, count: int = 1000):
+        """
+        метод для добавления всех найденных вакансий в общий список
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param count: общее количество вакансий (округляется кратно 100 в большую сторону), не более 1000
+        :return: список с данными вакансий со всех страниц или исключение GetReguestError
+        """
         pages = count // 100 if count % 100 == 0 else count // 100 + 1
         all_vacancies = []
         for page in range(pages):
@@ -52,13 +64,25 @@ class HH(Engine):
             all_vacancies.extend(vacancies_per_page)
         return all_vacancies
 
-    def save_to_json(self, keyword, path):
+    def save_to_json(self, keyword: str, path: str):
+        """
+        метод для сохранения полученных вакансий в файл .json
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param path: путь, по которому будет сохранен файл .json
+        :return: None
+        """
         with open(path, 'w', encoding='utf-8') as f:
             json.dump(self.get_vacancies(keyword), f, ensure_ascii=False, indent=4)
 
 
 class SuperJob(Engine):
-    def get_request(self, keyword, page):
+    def get_request(self, keyword: str, page: int):
+        """
+        метод отправки запроса и получения информации с сайта https://api.superjob.ru/2.0/vacancies/
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param page: текущая страница с результатами
+        :return: список с данными вакансий с текущей страницы или исключение GetReguestError
+        """
         url = 'https://api.superjob.ru/2.0/vacancies/'
         token = 'v3.r.117534731.e77433e8cea532f82a8b4705bb091839d01c48e7.fa0b7fdd9d180f28a4fe7c99c13a1a1909b59dbd'
         headers = {
@@ -68,7 +92,6 @@ class SuperJob(Engine):
             'Content-Type': 'application/x-www-form-urlencoded'
         }
         params = {
-            # "keywords": [{"keys": keyword, "srws": 1}],
             "count": 100,
             "keyword": keyword,
             "page": page
@@ -80,6 +103,12 @@ class SuperJob(Engine):
             raise GetReguestError(response.status_code, 'SuperJob')
 
     def get_vacancies(self, keyword: str, pages: int = 5):
+        """
+        метод для добавления всех найденных вакансий в общий список
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param pages: текущая страница с результатами
+        :return: список с данными вакансий со всех страниц или исключение GetReguestError
+        """
         all_vacancies = []
         for page in range(pages):
             print('Парсинг страницы', page + 1)
@@ -89,16 +118,29 @@ class SuperJob(Engine):
             all_vacancies.extend(vacancies_per_page)
         return all_vacancies
 
-    def save_to_json(self, keyword, path):
+    def save_to_json(self, keyword: str, path: str):
+        """
+        метод для сохранения полученных вакансий в файл .json
+        :param keyword: слово/слова, которые необходимо найти в тексте вакансии
+        :param path: путь, по которому будет сохранен файл .json
+        :return: None
+        """
         with open(path, 'w', encoding='utf-8') as f:
             json.dump(self.get_vacancies(keyword), f, ensure_ascii=False, indent=4)
 
 
 class Vacancies:
+    """Класс для представления вакансий. Атрибут класса all - список, который содержит все экземпляры класса"""
     all = list()
 
     @staticmethod
     def get_select_json_hh(path) -> list:
+        """
+        метод для выбора полей из файла .json с результатами парсинга с платформы HeadHunter,
+        неообходимых для инициализации экземпляра класса
+        :param path: путь до файла .json с результатами парсинга
+        :return: список словарей
+        """
         all_select_hh = list()
         with open(path, encoding="utf-8") as f:
             data = json.load(f)
@@ -124,6 +166,12 @@ class Vacancies:
 
     @staticmethod
     def get_select_json_sj(path) -> list:
+        """
+        метод для выбора полей из файла .json с результатами парсинга с платформы SuperJob,
+        неообходимых для инициализации экземпляра класса
+        :param path: путь до файла .json с результатами парсинга
+        :return: список словарей
+        """
         all_select_sj = list()
         with open(path, encoding="utf-8") as f:
             data = json.load(f)
@@ -141,6 +189,15 @@ class Vacancies:
 
     @classmethod
     def instantiate_from_json(cls, path_hh, path_sj, is_hh=True, is_sj=True):
+        """
+        метод инициализации экземпляра класса, данными из файлов .json с результатами парсинга для двух платформ.
+        при вызове данного метода, атрибут класса all предварительно очищается
+        :param path_hh: путь до файла .json с результатами парсинга с HeadHunter
+        :param path_sj: путь до файла .json с результатами парсинга с SuperJob
+        :param is_hh: указатель на платформу HH -> True - учитывать данные с платформы / False - не учитывать
+        :param is_sj: указатель на платформу SJ -> True - учитывать данные с платформы / False - не учитывать
+        :return: None
+        """
         cls.all.clear()
         if is_hh:
             for vac_hh in cls.get_select_json_hh(path_hh):
@@ -153,6 +210,11 @@ class Vacancies:
 
     @classmethod
     def sort_by_salary(cls, reverse=True):
+        """
+        метод сортировки экземпляров класса Vacancies внутри метода all.
+        :param reverse: True - сортировка от большей зарплаты к меньшей / False - от меньшей к большей
+        :return: атрибут класса all -> list
+        """
         cls.all.sort(key=lambda x: x.salary_max if x.salary_max else 0, reverse=reverse)
         cls.all.sort(key=lambda x: x.salary_min if x.salary_min else 0, reverse=reverse)
         return cls.all
@@ -170,8 +232,6 @@ class Vacancies:
     def __str__(self):
         str_salary_min = f'от {self.salary_min}' if self.salary_min else ''
         str_salary_max = f'до {self.salary_max}' if self.salary_max else ''
-        str_salary = f'{str_salary_min} {str_salary_max} {self.currency}' if self.salary_min or self.salary_max else 'не указана'
+        str_salary = f'{str_salary_min} {str_salary_max} {self.currency}' \
+            if self.salary_min or self.salary_max else 'не указана'
         return f'Вакансия с {self.platform}: {self.name} от компании {self.employer}\n{self.url}\nЗарплата {str_salary}'
-
-
-
